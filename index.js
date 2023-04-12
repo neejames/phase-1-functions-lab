52a573016968a47df3fa64c97ea8c52acb1838b3
Neechelle James <neechelle.james@gmail.com>
2023-04-12 14:32:16 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return (Math.abs(someValue - 42))
+  }
+  function distanceFromHqInFeet(someValue) {
+   return (Math.abs(distanceFromHqInBlocks(someValue) * 264))
+  }
+  function distanceTravelledInFeet(start, destination) {
+    return (Math.abs((destination - start) * 264))
+  }
+  function calculatesFarePrice(start, destination) {
+    let distanceTravelled = Math.abs(destination - start);
+    if (distanceTravelled * 264 <= 400) {
+      console.log('free sample');
+      return 0;
+    }
+    else if (400 < (distanceTravelled * 264) && (distanceTravelled * 264) <= 2000) {
+      let price = ((distanceTravelled * 264)- 400) * .02;
+      return price    
+    }
+    else if (2000 < (distanceTravelled * 264) && (distanceTravelled * 264) < 2500) {
+            return 25
+    }
+    else if ((distanceTravelled * 264) >= 2500) {
+      return 'cannot travel that far'
+    }
+  }
